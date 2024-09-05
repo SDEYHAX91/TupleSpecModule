ec668c7db171a8b763e59c1c7d62880746495ec0
SDEYHAX91 <sourjya.dey2004@gmail.com>
2024-09-05 12:03:18 +0530

    Create Tuple_remove_element.py

---
+++ Tuple_remove_element.py
@@ -0,0 +1,13 @@
+def t_remove(tup, element):  # Removes the first occurrence of the element in the tuple
+    if element not in tup:
+        raise ValueError(f"{element} not found in tuple")
+
+    for i in range(len(tup)):
+        if tup[i] == element:
+            tup = tup[:i] + tup[i+1:]
+            break
+    return tup
+
+tup = (3, 6, 9, 12, 15, 18)
+tp = t_remove(tup, 89)
+print(tp)
